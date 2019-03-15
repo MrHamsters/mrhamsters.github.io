46698a8f29247353b7e4f498b63371b4b4ff68c6
Jonathan Jensen <43424563+MrHamsters@users.noreply.github.com>
2019-03-15 08:44:28 -0400

    v0.8.3h test 2

--- ProjectInfinity.js
+++ ProjectInfinity.js
@@ -1,4 +1,4 @@
-var version="0.8.3h test";
+var version="0.8.3h test2";
 void setup(){
   size(1133,700);
   strokeWeight(10);
@@ -8601,10 +8601,10 @@ var convergingdialog={
 										res:0.85
 									}
 								};
-								player.activetraits.e=63;
 								loadtraits();
 								recalstats();
 								getstances();
+								player.activetraits.e=63;
 								convergingdialog.introarmor();
 							}
 						},
@@ -17404,8 +17404,7 @@ if(tick%10==0){
 		shape(playertemp.activetraitsprites.space,850,607,180,270);
 		fill(0,0,255);
 		textFont(0,15);
-		//text("SPACE",825,572);
-		text(player.activetraits.space,825,572);
+		text("SPACE",825,572);
 		if(traitcd[player.activetraits.space]>=30){
 			fill(0,0,0,130+playertemp.traitcd.space/traitcd[player.activetraits.space]*125);
 			ellipseMode(CENTER);
